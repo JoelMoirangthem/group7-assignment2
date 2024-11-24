88baed1b14f7cbdabbd14014b81557ab8341ca9c
Tamnna-Parveen <tamnnaparveen055@gmail.com>
2024-11-25 00:21:05 +0530

    Update LDU.js

--- LDU.js
+++ LDU.js
@@ -141,3 +141,18 @@ function L_Matrix(id) {
     document.getElementById(id).innerHTML += `<h2>EM matrix </h2>` + matrix_format(EM);
     document.getElementById(id).innerHTML += `<h2>L matrix </h2>` + matrix_format(L);
 }
+
+// Function to create a diagonal matrix of a given order
+function diagonal(id) {
+    D = [];
+    for (let i = 0; i < order; i++) {
+        let diagonal = [];
+        for (let j = 0; j < order; j++) {
+            diagonal.push(i === j ? U[i][j] : 0);
+        }
+        D.push(diagonal);
+    }
+    
+    // Display the D matrix in the specified HTML element
+    document.getElementById(id).innerHTML = <h2>D matrix </h2> + matrix_format(D);
+} 
