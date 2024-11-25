1c2c42a4fdc408225544a190926f578d2c1e4ad0
unknown <shubhamkumar47451@gmail.com>
2024-11-25 10:28:15 +0530

    some css and function to solve LDU

--- LDU.js
+++ LDU.js
@@ -156,3 +156,16 @@ function diagonal(id) {
     // Display the D matrix in the specified HTML element
     document.getElementById(id).innerHTML = <h2>D matrix </h2> + matrix_format(D);
 } 
+
+
+// Function to solve LDU
+function LDU(id) {
+    let X_matrix = X('upperMatrix');
+
+    // Multiply L, D, and X to get LDU
+    let first = mult_matrix(L, D, order); 
+    let second = mult_matrix(first, X_matrix, order); 
+    // Update the page with the LDU matrix
+    document.getElementById(id).innerHTML += <h2>LDU matrix </h2> + matrix_format(second);
+    document.getElementById(id).innerHTML += <h2>A matrix </h2> + matrix_format(A);
+}
